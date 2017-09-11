b37aaf344f60afcd6a029d4a09b67f6906c486c8
VincentCreveld <31699098+VincentCreveld@users.noreply.github.com>
2017-09-11 23:07:18 +0200

    Roosteropdracht les 1

---
+++ Project1/Project1/Functions.cpp
@@ -0,0 +1,8 @@
+#include "Functions.h"
+#include <string>
+using namespace std;
+
+
+void makeLessonBlock(int _lessonTime, int _weekNum, int _dayNum, int _lessonDuration, string _fNameTeacher, string _lNameTeacher, string _classes, string _classroomNo, string _moduleNaam) {
+	//Binnen deze functie wordt een Block object aangemaakt en toegevoegd aan de database
+}
---
+++ Project1/Project1/Functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void makeLessonBlock(int, int, int, int, string, string, string, string, string);
+
+#endif // !FUNCTIONS_H
