cbb347a7c35629b2ebaa0820b68681d0ba11df37
VincentCreveld <31699098+VincentCreveld@users.noreply.github.com>
2017-10-24 15:50:47 +0200

    Add files via upload

--- QueueSemantics/QueueSemantics/TemplateQueue.h
+++ QueueSemantics/QueueSemantics/TemplateQueue.h
@@ -1,72 +1,31 @@
 #pragma once
-# define my_sizeof(type) ((char *)(&type+1)-(char*)(&type))
+#include <vector>
 
 template<class T>
 class TemplateQueue{
 public:
 	TemplateQueue(T obj) {
-		objArray[0] = obj;
-		//objArray = new T[0];
-		//tempArray = T[0];
+		queue = new std::vector<T>();
 	}
 
 	void Put(T obj) {
-		//Add tempArray to heap
-		int arraySize = Size() + 1;
-		tempArray = T[arraySize];
-		//Fill tempArray with current array
-		for (int i = 0; i < Size(); i++)
-		{
-			tempArray[i] = objArray[i];
-		}
-
-		//Add new object to tempArray
-		tempArray[arraySize] = obj;
-
-		//Destroy current Array
-		destroy(objArray);
-
-		//Create new Array
-		objArray = T[arraySize] ;
-
-		//Set new Array equals to tempArray
-		objArray = tempArray;
+		queue.push_back(T);
 	}
+
 	T Get() {
 		T obj;
-
-		//Add tempArray to heap
-		int arraySize = Size() - 1;
-		tempArray = T[arraySize];
-		obj = objArray[0];
-
-		//Fill tempArray with current array
-		for (int i = 1; i < Size(); i++)
-		{
-			tempArray[i-1] = objArray[i];
-		}
-
-		//Destroy current Array
-		//destroy(objArray);
-
-		//Create new Array
-		objArray = T[arraySize] ;
-
-		//Set new Array equals to tempArray
-		objArray = tempArray;
-
+		obj = queue.front();
+		queue.erase(queue.front());
 		return obj;
 	}
 
 	T Peek() {
-		return objArray[0];
+		return queue.front();
 	}
 
 	int Size() {
-		int size = my_sizeof(objArray) / my_sizeof(objArray[0]);
-		return size;
+		return queue.size();
 	}
 private: 
-	T objArray[1];
-	T tempArray[1];
+	std::vector<T> queue;
 };
